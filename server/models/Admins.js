718d5695fe356447d053a4573d6b7f38348d6e71
Khaled Alkilani <85066014+KhaledAlkilani@users.noreply.github.com>
2023-05-06 23:08:05 +0300

    Update Admins.js
    Comments and explanation for the Admins model codes

--- server/models/Admins.js
+++ server/models/Admins.js
@@ -1,6 +1,12 @@
+//imports the Schema and model objects from the "mongoose" library after installing mongoose package
 const {Schema, model} = require("mongoose")
 
+//Create a new Mongoose schema called AdminSchema using the imported Schema object. 
+//A schema is a blueprint that defines the structure and properties of a MongoDB document.
 const AdminSchema = new Schema({
+
+//Define two properties within the AdminSchema: username and password. 
+//Each property is an object that specifies its type, required status, and any additional validations or options.
     username: {
         type: String,
         required: true,
@@ -13,6 +19,11 @@ const AdminSchema = new Schema({
   
 })
 
+//Create a Mongoose model called AdminModel using the imported model function. 
+//The model represents a collection in the MongoDB database and provides an interface for interacting with it. 
+//It takes two arguments: the name of the collection ("admins") and the schema (AdminSchema).
 const AdminModel = model("admins", AdminSchema)
 
+//Export the AdminModel so that it can be used in other parts of the code. 
+//It allows other modules to import and use the AdminModel when interacting with the "admins" collection in the MongoDB database.
 module.exports = AdminModel
